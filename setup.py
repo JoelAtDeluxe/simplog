fa6ad95ba28d84aa774fca9bca83e79cc97ee823
Joel Smith <joel.smith@bydeluxe.com>
2019-05-15 09:59:22 -0700

    setup updates

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from distutils.core import setup
 
+with open('README.md', 'r') as fh:
+  long_description=fh.read()
+
 setup(
   name = 'simplog',
   packages = ['simplog'],
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
-  description = 'A simple, functional, opinionated logger for python',   
+  description = 'A simple, functional, opinionated logger for python',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   author = 'Joel Smith',
   author_email = 'joel.pypi@gmail.com',
   url = 'https://github.com/JoelAtDeluxe/simplog',
-  download_url = 'https://github.com/JoelAtDeluxe/simplog/archive/v0.0.1.tar.gz', 
+  download_url = 'https://github.com/JoelAtDeluxe/simplog/archive/v0.0.2.tar.gz', 
   keywords = ['logging', 'functional', 'splunk'],
   install_requires=[],
   classifiers=[
+    'Programming Language :: Python :: 3',
     'Development Status :: 4 - Beta',
+    "Operating System :: OS Independent",
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3', 
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',    
   ],
 )
